513951f408819eb8a8ab642cc2b06d484025ecd0
BlackDovah <132620985+BlackDovah@users.noreply.github.com>
2025-02-01 20:46:36 +0200

    Update projects.ts

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -1,9 +1,9 @@
 export const projects = {
   1: {
-    title: "NoThanks boardgame",
-    description: "A boardgame created using React and BoardGame.io.",
-    image: "/images/BoardGameUI.png",
-    link: "https://github.com/BlackDovah/NoThanksBoardGame"
+    title: "BookStore",
+    description: "My Biggest project yet, with more planned updates to come. A fullstack project featuring a frontend portion created using the mantine component library for react, typsecript, and tailwindcss. The backend portion is created using express.js and node.js.",
+    image: "/images/BookStore.png",
+    link: "https://github.com/BlackDovah/BookStore"
   },
   2: {
     title: "React simple calculator",
@@ -12,9 +12,15 @@ export const projects = {
     link: "https://github.com/BlackDovah/ReactCalculator"
   },
   3: {
-    title: "BookStore",
-    description: "A fullstack project featuring a frontend portion created using the mantine component library for react, typsecript, and tailwindcss. The backend portion is created using express.js and node.js.",
-    image: "/images/BookStore.png",
-    link: "https://github.com/BlackDovah/BookStore"
+    title: "NoThanks boardgame",
+    description: "A boardgame created using React and BoardGame.io.",
+    image: "/images/BoardGameUI.png",
+    link: "https://github.com/BlackDovah/NoThanksBoardGame"
+  },
+  4: {
+    title: "BookStore Backend",
+    description: "A backend project created using express.js and node.js. This project that I made during a backend development course taken with IBM on coursera is a book review website where users can create an account and review books. The account information is saved in a users array, and is always referenced if the user tries to login to determin if an account does exist or not, and a token is created for authenticated users that have an account to allow them to access authenticated user only functions that allows them to make or delete reviews. Non-registerd users have access to limited functions, that allows them to browse books, and view their reviews, but without the ability to leave their own reviews.",
+    image: "https://booksliced.com/books/wp-content/uploads/2019/01/iMAGE_Our_Top_20_Online_Bookstores_and_Where_to_Find_Every_Online_Bookstore_-768x510.jpg",
+    link: "https://github.com/BlackDovah/expressBookReviews"
   }
 }
