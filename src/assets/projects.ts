6c775e48e513dc874bed9e08cfbccbfb3d054d15
BlackDovah <132620985+BlackDovah@users.noreply.github.com>
2025-02-01 06:04:19 +0200

    Update projects.ts

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -2,19 +2,19 @@ export const projects = {
   1: {
     title: "NoThanks boardgame",
     description: "A boardgame created using React and BoardGame.io.",
-    image: "/public/images/BoardGameUI.png",
+    image: "/images/BoardGameUI.png",
     link: "https://github.com/BlackDovah/NoThanksBoardGame"
   },
   2: {
     title: "React simple calculator",
     description: "A calculator created using React and tailwindcss as my very first project.",
-    image: "/public/images/reactCalculator.png",
+    image: "/images/reactCalculator.png",
     link: "https://github.com/BlackDovah/ReactCalculator"
   },
   3: {
     title: "BookStore",
     description: "A fullstack project featuring a frontend portion created using the mantine component library for react, typsecript, and tailwindcss. The backend portion is created using express.js and node.js.",
-    image: "/public/images/BookStore.png",
+    image: "/images/BookStore.png",
     link: "https://github.com/BlackDovah/ReactCalculator"
   }
 }
